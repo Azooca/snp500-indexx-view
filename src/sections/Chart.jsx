434917ba6e9368dfe9f829690a4a94209c94ae33
Emm Kwami <edenated@gmail.com>
2022-06-02 21:44:00 -0700

    working

--- src/sections/Chart.jsx
+++ src/sections/Chart.jsx
@@ -19,8 +19,7 @@ function ChartSNP() {
    
     const buildData = (period = '1d')=>{ 
       
-        fetch('https://coinx500.io/?period='+period, {
-          
+        fetch('http://127.0.0.1:8000/?period='+period, {
           method:'GET', 
           headers: {
             'Content-Type': 'application/json',
