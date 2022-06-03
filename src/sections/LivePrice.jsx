97481b5e5f2914077b9349da24a807e5e9726685
Emm Kwami <edenated@gmail.com>
2022-06-03 12:06:09 -0700

    update

--- src/sections/LivePrice.jsx
+++ src/sections/LivePrice.jsx
@@ -14,7 +14,7 @@ function LivePrice() {
     .then((response)=>response.json())
     .then((data)=>{
         let p = parseFloat(data['price'])
-        let price = (p / 1000).toLocaleString("en-US")
+        let price = ((p / 1000).toLocaleString("en-US")).toFixed(2)
         setPrice(price) 
     })
 }
@@ -27,7 +27,7 @@ useEffect(()=>{
         <h2 className='font-medium text-2xl text-center text-primary'>Indexx Stock Token Price
         </h2>
         <h2 className='font-medium text-6xl text-center my-5'>
-        {price}
+        ${price}
         </h2>
         <div className='flex items-center justify-center m-auto'>
             <button className='bg-secondary text-primary rounded-3xl p-3 inline-block align-middle shadow-lg w-full'>
